bed95b7ad242a01387dce44b9d255953bb5c35ef
optiphil <87368971+optiphil@users.noreply.github.com>
2021-07-14 03:52:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
         <div class="nav-links">
         <ul>
             <li><a href="#">HOME</a></li>
-            <li><a href="#">ABOUT US</a></li>
-            <li><a href="#">BLOG</a></li>
+            <li><a href="./About-us.php">ABOUT US</a></li>
+            <li><a href="#">BLOG</a></li>2
             <li><a href="#">CONTACT</a></li>
         </ul>
     </div>
